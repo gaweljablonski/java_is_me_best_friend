14ff0cb6d9acc90ac42a6b881621242df46fc0b4
gaweljablonski <gawel.jablonski2006@gmail.com>
2023-12-03 22:25:05 +0100

    Udało sie połączyć do MySQL

--- kursUdemy/src/testowe/connectMySQL/MainActivity.java
+++ kursUdemy/src/testowe/connectMySQL/MainActivity.java
@@ -4,16 +4,57 @@ import java.sql.*;
 
 public class MainActivity {
     public static void main(String[] args) {
-        String host = "jdbc:mysql://127.0.0.1:3306/employee101";
-        String username = "root@127.0.0.1";
+        String host = "jdbc:mysql://localhost:3306/employee101";
+        String username = "root";
         String password = "";
+
+        Connection connection = null;
+
+
         try {
-            System.out.println("czyjednak");
-            Connection conn = DriverManager.getConnection(host, username, password);
-            System.out.println("Connected to MySQL database");
-        } catch (SQLException e) {
-            System.out.println("Failed to connect to MySQL database");
-            e.printStackTrace();
+            // load and register JDBC driver for MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establishing a connection to the database
+            connection = DriverManager.getConnection(host, username, password);
+            System.out.println("Connected to the database!");
+
+            // Creating a statement
+            Statement statement = connection.createStatement();
+
+            // SQL query to retrieve data
+            String sqlQuery = "SELECT id, name, age FROM employee_data";
+
+            // Executing the query
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            // Processing the result set
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int salary = resultSet.getInt("age");
+
+                // Print retrieved values
+                System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary);
+            }
+
+            // Closing resources
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("Connection failed: " + e.getMessage());
+        } finally {
+            // Close the connection in the finally block to ensure it's closed properly
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                    System.out.println("Connection closed.");
+                }
+            } catch (SQLException e) {
+                System.err.println("Error closing the connection: " + e.getMessage());
+            }
         }
     }
 }
